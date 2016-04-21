2931b1d505ccba9f730516f152518db1212606a9
nonoroazoro <xiaochao.k@gmail.com>
2016-04-21 13:36:53 +0800

    add test

---
+++ test/lib/index.test.js
@@ -0,0 +1,14 @@
+﻿const mocha = require("mocha");
+const should = require("should");
+
+const ss = require("../../");
+
+describe("lib/index", () =>
+{
+    describe("1.boyer-moor", () =>
+    {
+        it("should find string ", (done) =>
+        {
+        });
+    });
+});
