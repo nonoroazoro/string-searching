d7a0326914e18b3a8ff0680e12d765b199d86c10
nonoroazoro <xiaochao.k@gmail.com>
2016-04-22 14:32:44 +0800

    update

--- test/lib/index.test.js
+++ test/lib/index.test.js
@@ -1,15 +1,42 @@
 ﻿const mocha = require("mocha");
 const should = require("should");
 
-const ss = require("../../");
+const stringSearching = require("../../");
 
 describe("lib/index", () =>
 {
     describe("1.boyer-moor", () =>
     {
-        it("should find string ", (done) =>
+        it("should not find string ", (done) =>
         {
-            console.log(ss.boyer_moore("HERE IS A SIMPLE EXAMPLE", "EXAMPLE"));
+            const nullString = null;
+            const arrayString = [1, 2];
+            const okString = "example";
+
+            stringSearching.boyer_moore(nullString, arrayString).should.equal(-1);
+            stringSearching.boyer_moore(nullString, okString).should.equal(-1);
+            stringSearching.boyer_moore(arrayString, nullString).should.equal(-1);
+            stringSearching.boyer_moore(arrayString, okString).should.equal(-1);
+            stringSearching.boyer_moore(okString, nullString).should.equal(-1);
+            stringSearching.boyer_moore(okString, arrayString).should.equal(-1);
+            done();
+        });
+
+        const targetIndex1 = 17;
+        const pattern1 = "EXAMPLE";
+        const text1 = "HERE IS A SIMPLE EXAMPLE";
+        it(`should find string "${pattern1}" in "${text1}" \n\t and the index is ${targetIndex1}`, (done) =>
+        {
+            stringSearching.boyer_moore(text1, pattern1).should.equal(targetIndex1);
+            done();
+        });
+
+        const targetIndex2 = [13, 21];
+        const pattern2 = "PLE";
+        const text2 = "HERE IS A SIMPLE EXAMPLE";
+        it(`should find string "${pattern2}" in "${text2}" \n\t and the index is ${targetIndex2}`, (done) =>
+        {
+            stringSearching.boyer_moore(text2, pattern2, true).should.deepEqual(targetIndex2);
             done();
         });
     });
