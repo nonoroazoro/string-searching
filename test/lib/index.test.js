88d4337e8eadd0e0b335a95804c2f8da0dd75689
nonoroazoro <xiaochao.k@gmail.com>
2016-04-22 14:45:39 +0800

    remove arrow functions

--- test/lib/index.test.js
+++ test/lib/index.test.js
@@ -3,11 +3,11 @@ const should = require("should");
 
 const stringSearching = require("../../");
 
-describe("lib/index", () =>
+describe("lib/index", function ()
 {
-    describe("1.boyer-moor", () =>
+    describe("1.boyer-moor", function ()
     {
-        it("should not find string ", (done) =>
+        it("should not find string ", function (done)
         {
             const nullString = null;
             const arrayString = [1, 2];
@@ -25,7 +25,7 @@ describe("lib/index", () =>
         const targetIndex1 = 17;
         const pattern1 = "EXAMPLE";
         const text1 = "HERE IS A SIMPLE EXAMPLE";
-        it(`should find string "${pattern1}" in "${text1}" \n\t and the index is ${targetIndex1}`, (done) =>
+        it(`should find string "${pattern1}" in "${text1}" \n\t and the index is ${targetIndex1}`, function (done)
         {
             stringSearching.boyer_moore(text1, pattern1).should.equal(targetIndex1);
             done();
@@ -34,7 +34,7 @@ describe("lib/index", () =>
         const targetIndex2 = [13, 21];
         const pattern2 = "PLE";
         const text2 = "HERE IS A SIMPLE EXAMPLE";
-        it(`should find string "${pattern2}" in "${text2}" \n\t and the index is ${targetIndex2}`, (done) =>
+        it(`should find string "${pattern2}" in "${text2}" \n\t and the index is ${targetIndex2}`, function (done)
         {
             stringSearching.boyer_moore(text2, pattern2, true).should.deepEqual(targetIndex2);
             done();
