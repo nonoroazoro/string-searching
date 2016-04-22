f01ef8cfa07a6acac78f1ca86de517931449d2f1
nonoroazoro <xiaochao.k@gmail.com>
2016-04-22 16:34:52 +0800

    add performance test

---
+++ test/performance.test.js
@@ -0,0 +1,28 @@
+﻿const mocha = require("mocha");
+const should = require("should");
+
+const stringSearching = require("../");
+const text = require("./performance.test.json").content;
+
+describe.only("performance-test", () =>
+{
+    const pattern = "工艺";
+
+    it("should finish performance-test with: boyer_moore", (done) =>
+    {
+        const time = process.hrtime();
+        stringSearching.boyer_moore(text, pattern);
+        const diff = process.hrtime(time);
+        console.log("\tsearch took %d microseconds", (diff[0] * 1e9 + diff[1]) / 1e3);
+        done();
+    });
+
+    it("should finish performance-test with: indexOf", (done) =>
+    {
+        const time = process.hrtime();
+        text.indexOf(pattern);
+        const diff = process.hrtime(time);
+        console.log("\tsearch took %d microseconds", (diff[0] * 1e9 + diff[1]) / 1e3);
+        done();
+    });
+});
