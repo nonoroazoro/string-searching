eb551f703a80aa28382d0f75893ad82c62c73eee
nonoroazoro <xiaochao.k@gmail.com>
2016-04-22 16:54:25 +0800

    update

--- test/performance.test.js
+++ test/performance.test.js
@@ -7,22 +7,27 @@ const text = require("./performance.test.json").content;
 describe.only("performance-test", () =>
 {
     const pattern = "工艺";
+    const targetIndex = 5790;
 
     it("should finish performance-test with: boyer_moore", (done) =>
     {
         const time = process.hrtime();
-        stringSearching.boyer_moore(text, pattern);
+        const index = stringSearching.boyer_moore(text, pattern);
         const diff = process.hrtime(time);
         console.log("\tsearch took %d microseconds", (diff[0] * 1e9 + diff[1]) / 1e3);
+
+        targetIndex.should.equal(index);
         done();
     });
 
     it("should finish performance-test with: indexOf", (done) =>
     {
         const time = process.hrtime();
-        text.indexOf(pattern);
+        const index = text.indexOf(pattern);
         const diff = process.hrtime(time);
         console.log("\tsearch took %d microseconds", (diff[0] * 1e9 + diff[1]) / 1e3);
+
+        targetIndex.should.equal(index);
         done();
     });
 });
